f31dd7dfa6e3c8399ce13104b4f9dce757c330a4
trevor grayson <trevor@trevorgrayson.com>
2023-03-19 00:57:41 -0700

    calcboard

---
+++ projects/calcboard/src/calculator.h
@@ -0,0 +1,63 @@
+// int isNum(char key); 
+// int isOperator(char key);
+// void calcPress(char key);
+// float calcDisplay(char key);
+float reg0 = 0;
+float reg1 = 0;
+char operatr = 0;
+
+
+int isNum(char key) {
+  if(key < 58 && key > 47) {
+    return 1;
+  }
+  return 0;
+}
+
+int isOperator(char key) {
+  if(key < 48 && key > 41) {
+    return 1;
+  }
+  return 0;
+}
+
+float operate() {
+  switch(operatr) {
+    case '*': return reg1 * reg0;
+    case '/': return reg1 / reg0;
+    case '+': return reg1 + reg0;
+    case '-': return reg1 - reg0;
+  }
+  return reg1;
+}
+
+void calcPress(char key) {
+  if(isNum(key)) {
+    reg0 = reg0 * 10 + (key - 48);
+  } else if(isOperator(key)) {
+    operatr = key;
+    if(reg0 == 0) {
+      return;
+    } 
+
+    if(reg1 == 0) {
+        reg1 = reg0;
+    } else {
+        reg1 = operate();
+    }
+    reg0 = 0;
+  } else if(key == '#') { // CLEAR
+    reg0 = 0;
+  }
+}
+
+float calcDisplay(char key) {
+  if(isNum(key)) {
+    return reg0;
+  } else if(isOperator(key)) {
+    return reg1;
+  } else if(key == '#') { // ENTER
+    //Serial.println(reg0, reg1);
+  }
+  return 0;
+}
