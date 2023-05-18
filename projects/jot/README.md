59bffb9a15ad18eeec4cca36c7e5853d84c84c05
trevor grayson <trevor@trevorgrayson.com>
2023-05-17 23:19:26 -0700

    keyboard appeared to be working briefly

---
+++ projects/jot/README.md
@@ -0,0 +1,14 @@
+# jot
+
+`esp01` with a M5 cardkbd over I2C.
+
+| esp01    | cardkbd (GROVE) |
+|----------|-----------------|
+| VCC 3.3V | 3.3V (Red)    3 |
+| GND      | GND  (Black)  4 | 
+| 0        | SCL  (Yellow) 1 |
+| 2        | SDA  (White)  2 | 
+
+[CardKBD](https://shop.m5stack.com/products/cardkb-mini-keyboard?variant=32007609319514)
+[esp01](https://www.microchip.ua/wireless/esp01.pdf)
+[mega328P](https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf)
--- projects/jot/src/main.cpp
+++ projects/jot/src/main.cpp
@@ -2,20 +2,35 @@
 #include <Wire.h>
 #include "display.h"
 
+#define LED_PIN 2
+#define SDA_PIN 2
+#define SCL_PIN 0
 
 void setup() {
-  Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+    //pinMode(LED_PIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+    Wire.pins(SDA_PIN, SCL_PIN);
+    Wire.begin(SDA_PIN, SCL_PIN);        // join i2c bus (address optional for master)
+    Serial.begin(9600);  // start serial for output
+    delay(1000);
+    Serial.print("start. ");
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from peripheral device #8
+    Wire.requestFrom(0x5F, 6);    // request 6 bytes from peripheral device #8
+    Serial.print(".");
+    while (Wire.available()) { // peripheral may send less than requested
+        char c = Wire.read(); // receive a byte as character
+        Serial.print(c);         // print the character
+        Serial.print("*");
+//        Serial.print("*");
 
-  while (Wire.available()) { // peripheral may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }
+//        digitalWrite(LED_PIN, LOW);
+//        delay(1000);
+//        digitalWrite(LED_PIN, HIGH);
+//        delay(1000);
+    }
 
-  delay(500);
+    delay(500);
 }
 // https://github.com/gamegine/HCSR04-ultrasonic-sensor-lib
