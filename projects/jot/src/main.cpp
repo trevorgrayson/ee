623c58dabaab03412c882c3cc3a7b7d15d6cbde1
trevor grayson <trevor@trevorgrayson.com>
2023-06-01 16:36:33 -0700

    jot hardware MVP

--- projects/jot/src/main.cpp
+++ projects/jot/src/main.cpp
@@ -1,36 +1,21 @@
 #include <Arduino.h>
+#define CARDKB_ADDR 0x5F  //Define the I2C address of CardKB.
 #include <Wire.h>
-#include "display.h"
-
-#define LED_PIN 2
-#define SDA_PIN 2
-#define SCL_PIN 0
 
 void setup() {
-    //pinMode(LED_PIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-
-    Wire.pins(SDA_PIN, SCL_PIN);
-    Wire.begin(SDA_PIN, SCL_PIN);        // join i2c bus (address optional for master)
-    Serial.begin(9600);  // start serial for output
+    Wire.begin();
+    Serial.begin(9600);
     delay(1000);
-    Serial.print("start. ");
+    Serial.print("Start.");
 }
-
-void loop() {
-    Wire.requestFrom(0x5F, 6);    // request 6 bytes from peripheral device #8
-    Serial.print(".");
-    while (Wire.available()) { // peripheral may send less than requested
-        char c = Wire.read(); // receive a byte as character
-        Serial.print(c);         // print the character
-        Serial.print("*");
-//        Serial.print("*");
-
-//        digitalWrite(LED_PIN, LOW);
-//        delay(1000);
-//        digitalWrite(LED_PIN, HIGH);
-//        delay(1000);
+void loop()
+{
+    Wire.requestFrom(CARDKB_ADDR, 1); //Request 1 byte from the slave device.
+    while (Wire.available()) {
+        char c = Wire.read();
+        if (c != 0)
+        {
+            Serial.print(c);
+        }
     }
-
-    delay(500);
 }
-// https://github.com/gamegine/HCSR04-ultrasonic-sensor-lib
