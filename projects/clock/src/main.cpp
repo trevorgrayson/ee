0a4f92024971040240c983cf897930aaf8cd6304
trevor grayson <trevor@dave.com>
2023-06-25 22:43:25 -0700

    clock: a clock starting at 12:34

--- projects/clock/src/main.cpp
+++ projects/clock/src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 #include <TM1637.h>
 
+#define ONE_DAY  (24 * 60 * 60)
+
 const uint8_t colonMask = 0b01000000;
 
 // Instantiation and pins configurations
 // Pin 3 - > DIO
 // Pin 2 - > CLK
 TM1637 tm(2, 3);
+
+double epic = 12 * 3600 + 34 * 60; // seconds
 
 void setup()
 {
@@ -16,7 +20,14 @@ void setup()
 void loop()
 {
     // Display Integers:
-    tm.display(1234, colonMask);
+    int hours = floor(epic / 3600);
+    int minutes = floor((epic - (hours * 3600)) / 60);
+
+    tm.display(hours * 100 + minutes, colonMask);
     // display.showNumberDecEx(hourFormat12(timeinfo.tm_hour), colonMask, false, 2, upperDigits);
+    epic++;
+    if (epic > ONE_DAY) {
+        epic = 0000;
+    }
     delay(1000);
 }
