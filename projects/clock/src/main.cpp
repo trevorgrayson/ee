8a6140a0610fc6427bffa27cdb983d366836cfe1
trevor <trevor@trevorgrayson.com>
2023-07-26 00:18:16 -0400

    clock, still sucks. arduino crystal doesn't exist

--- projects/clock/src/main.cpp
+++ projects/clock/src/main.cpp
@@ -1,33 +1,56 @@
 #include <Arduino.h>
-#include <TM1637.h>
+#include <TM1637Display.h>
 
-#define ONE_DAY  (24 * 60 * 60)
 
-const uint8_t colonMask = 0b01000000;
+//#define ONE_DAY  (24 * 60 * 60)
+double ONE_DAY =  (24.0 * 60.0 * 60.0);
+const uint8_t colonMask = 0b11100000;
+const double MINUTES = 60.0;
+const double HOURS = 3600.0;
+
+
+// set the time
+double epic = 21.0 *HOURS + 29.0 *MINUTES; // seconds
 
 // Instantiation and pins configurations
 // Pin 3 - > DIO
 // Pin 2 - > CLK
-TM1637 tm(2 , 15);
+TM1637Display display(2, 3);
 
-double epic = 12 * 3600 + 34 * 60; // seconds
+
+double tick_epic = millis();
+
 
 void setup()
 {
-    tm.begin();
+    Serial.begin(9600);
+    display.setBrightness(0x0a);
 }
 
 void loop()
 {
-    // Display Integers:
-    int hours = floor(epic / 3600);
-    int minutes = floor((epic - (hours * 3600)) / 60);
+    uint8_t toMask = 0b0;
 
-    tm.display(hours * 100 + minutes, colonMask);
-    // display.showNumberDecEx(hourFormat12(timeinfo.tm_hour), colonMask, false, 2, upperDigits);
-    epic++;
-    if (epic > ONE_DAY) {
-        epic = 0000;
+    // Display Integers:
+    double hours = floor(epic/3600.0);
+    double minutes = floor((epic - (hours * 3600.0)) / 60.0);
+    Serial.println(epic);
+    Serial.println(hours);
+    Serial.println(minutes);
+
+    if (millis() - tick_epic > 1000) {
+        tick_epic = millis();
+        epic++;
+        if (toMask) {
+            toMask = 0b0;
+        } else {
+            toMask = colonMask;
+        }
+
+        display.showNumberDecEx(hours * 100.0 + minutes, toMask);
     }
-    delay(1000);
+
+    if (epic > ONE_DAY) {
+        epic = 0000.0;
+    }
 }
