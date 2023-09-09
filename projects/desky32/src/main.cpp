f48d5e4fedc55a19fe8ecea6f7313d4a0ce8e9f6
trevor grayson <trevor@dave.com>
2023-09-08 23:14:18 -0700

    desky32 pins.h

--- projects/desky32/src/main.cpp
+++ projects/desky32/src/main.cpp
@@ -17,17 +17,6 @@
 #include "ThermalPrinter.h"
 #include "btcalc.h"
 
-#define OPER_MODE 0
-#define CALC_MODE 1
-#define KEYPAD_MODE 2
-#define DESKY 3
-
-#define DEBOUNCE_DELAY 50
-
-// display
-#define TOP_LINE 0
-#define BTM_LINE 1
-
 
 char cmd[] = "                    ";
 int offset = 0;
--- projects/desky32/src/pins.h
+++ projects/desky32/src/pins.h
@@ -3,3 +3,14 @@
 //
 
 #define SEND_PIN 13
+
+#define OPER_MODE 0
+#define CALC_MODE 1
+#define KEYPAD_MODE 2
+#define DESKY 3
+
+#define DEBOUNCE_DELAY 50
+
+// display
+#define TOP_LINE 0
+#define BTM_LINE 1
