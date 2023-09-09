2c2a2f42afd9a0f583cb25fb7237148ab20f7f2e
trevor grayson <trevor@trevorgrayson.com>
2023-09-08 19:40:22 -0700

    pins.h

--- projects/desky32/src/main.cpp
+++ projects/desky32/src/main.cpp
@@ -18,16 +18,6 @@
 #include "ThermalPrinter.h"
 
 
-#define CALC_MODE 0
-#define DSKY_MODE 1
-#define OPERATE 0
-
-#define DEBOUNCE_DELAY 50
-
-// display
-#define TOP_LINE 0
-#define BTM_LINE 1
-
 BleKeyboard bleKeyboard("DeSKY", "tg.", 100);
 
 char cmd[] = "                    ";
@@ -85,13 +75,6 @@ void output(int line, double value) {
 
 void setup() {
     Serial.begin(9600);
-//    while (Serial.available()) {
-//        Serial.read();
-//    }
-
-//    setupThermalPrinter();
-//    receiptPrint("hello world");
-//    footer();
 
     setupLCD();
     print("hello.");
--- projects/desky32/src/pins.h
+++ projects/desky32/src/pins.h
@@ -3,3 +3,14 @@
 //
 
 #define SEND_PIN 13
+
+#define CALC_MODE 0
+#define DSKY_MODE 1
+#define OPERATE 0
+
+#define DEBOUNCE_DELAY 50
+
+// display
+#define TOP_LINE 0
+#define BTM_LINE 1
+
