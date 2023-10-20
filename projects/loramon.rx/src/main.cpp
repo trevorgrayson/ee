3ba5e263bebc9ea11416098df21e55df77471d3a
Trevor Grayson <trevor@ipsumllc.com>
2023-10-20 01:20:25 -0400

    LoRa.RX building, unsure of pins

--- projects/loramon.rx/src/main.cpp
+++ projects/loramon.rx/src/main.cpp
@@ -1,25 +1,61 @@
-/*https://github.com/sandeepmistry/arduino-LoRa/tree/master/examples
- *
- *
- */
-// http://community.heltec.cn/t/lorawan-preamble-length-undeclared/11484/6
-#define LORAWAN_PREAMBLE_LENGTH 8
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
 
-#include <Wire.h>
 #include <SPI.h>
 #include <LoRa.h>
 
+//define the pins used by the transceiver module
+#define ss 8
+#define rst 12
+#define dio0 14  // 2
+
+//static const uint8_t SDA_OLED = 17;
+//static const uint8_t SCL_OLED = 18;
+//static const uint8_t RST_OLED = 21;
+//
+//static const int8_t SW_LoRa = -1;
+//static const uint8_t SS_LoRa = 8;
+//static const uint8_t SCK_LoRa = 9;
+//static const uint8_t MOSI_LoRa = 10;
+//static const uint8_t MISO_LoRa = 11;
+//static const uint8_t RST_LoRa = 12;
+//static const uint8_t BUSY_LoRa = 13;
+//static const uint8_t DIO1_LoRa = 14;
+//
+//It is also recommended to redefine Arduino default SPI-1 and I2C-1 pins to OLED & LoRa pins as follow:
+//
+//static const uint8_t SDA = 17; // instead of 8;
+//static const uint8_t SCL = 18; // instead of 9;
+//
+//static const uint8_t SS = 8; // instead of 10;
+//static const uint8_t MOSI = 10; // instead of 11;
+//static const uint8_t MISO = 11; // instead of 13;
+//static const uint8_t SCK = 9; // instead of 12;
 
 void setup() {
-    Serial.begin(9600);
+    //initialize Serial Monitor
+    Serial.begin(115200);
     while (!Serial);
-
     Serial.println("LoRa Receiver");
 
-    if (!LoRa.begin(915E6)) {
-        Serial.println("Starting LoRa failed!");
-        while (1);
+    //setup LoRa transceiver module
+    LoRa.setPins(ss, rst, dio0);
+
+    //replace the LoRa.begin(---E-) argument with your location's frequency
+    //433E6 for Asia
+    //866E6 for Europe
+    //915E6 for North America
+    while (!LoRa.begin(915E6)) {
+        Serial.println(".");
+        delay(500);
     }
+    // Change sync word (0xF3) to match the receiver
+    // The sync word assures you don't get LoRa messages from other LoRa transceivers
+    // ranges from 0-0xFF
+    LoRa.setSyncWord(0xF3);
+    Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
@@ -31,7 +67,8 @@ void loop() {
 
         // read packet
         while (LoRa.available()) {
-            Serial.print((char)LoRa.read());
+            String LoRaData = LoRa.readString();
+            Serial.print(LoRaData);
         }
 
         // print RSSI of packet
