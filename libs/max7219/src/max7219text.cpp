f9a49c43258319fc722225caebd60cf49c3451ee
trevor grayson <trevor@trevorgrayson.com>
2025-01-19 23:18:08 -0800

    max7219text lib

---
+++ libs/max7219/src/max7219text.cpp
@@ -0,0 +1,25 @@
+//
+// Created by trevor on 7/7/24.
+//
+
+#include "max7219text.h"
+
+void display(char *s)
+{
+
+}
+
+void display(int x)
+{
+
+}
+
+// void display(float x)
+// {
+//
+// }
+//
+// void display(double x)
+// {
+//
+// }
---
+++ libs/max7219/src/max7219text.h
@@ -0,0 +1,9 @@
+//
+// Created by trevor on 7/7/24.
+//
+
+void display(char *s);
+
+void display(int x);
+void display(float x);
+void display(double x);
