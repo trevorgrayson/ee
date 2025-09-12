a9ffcc5427b61652522ee33bfe03578703fa6127
trevor grayson <trevor@trevorgrayson.com>
2025-09-11 22:21:45 -0700

    deskcam with seeed esp32cam

--- reference/espeye/src/main.cpp
+++ reference/espeye/src/main.cpp
@@ -37,24 +37,6 @@ boolean takeNewPhoto = false;
 // Photo File Name to save in SPIFFS
 #define FILE_PHOTO "/photo.jpg"
 
-// OV2640 camera module pins (CAMERA_MODEL_AI_THINKER)
-// #define PWDN_GPIO_NUM     32
-// #define RESET_GPIO_NUM    -1
-// #define XCLK_GPIO_NUM      0
-// #define SIOD_GPIO_NUM     26
-// #define SIOC_GPIO_NUM     27
-// #define Y9_GPIO_NUM       35
-// #define Y8_GPIO_NUM       34
-// #define Y7_GPIO_NUM       39
-// #define Y6_GPIO_NUM       36
-// #define Y5_GPIO_NUM       21
-// #define Y4_GPIO_NUM       19
-// #define Y3_GPIO_NUM       18
-// #define Y2_GPIO_NUM        5
-// #define VSYNC_GPIO_NUM    25
-// #define HREF_GPIO_NUM     23
-// #define PCLK_GPIO_NUM     22
-
 // ESP EYE
 #define PWDN_GPIO_NUM    -1
 #define RESET_GPIO_NUM   -1
@@ -220,7 +202,11 @@ void setup() {
         config.jpeg_quality = 12;
         config.fb_count = 1;
     }
-    config.frame_size = FRAMESIZE_UXGA;
+    config.frame_size = FRAMESIZE_UXGA;   // 1600x1200
+    config.pixel_format = PIXFORMAT_JPEG; // Required at high res
+    config.jpeg_quality = 10;             // 0 = highest, 63 = lowest
+    config.fb_count = 2;                  // 2 frame buffers for smoother capture
+
     // Camera init
     esp_err_t err = esp_camera_init(&config);
     if (err != ESP_OK) {
