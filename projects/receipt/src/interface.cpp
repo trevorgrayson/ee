44cfc23d2e66b9cd17b33a614f5fe76fca3289ff
trevor grayson <trevor@trevorgrayson.com>
2025-10-15 20:06:49 -0700

    TODOne ai receipt mega app

--- projects/receipt/src/interface.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by trevor on 1/6/24.
-//
-
-#include "interface.h"
--- projects/receipt/src/txtings.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Trevor Grayson on 6/24/23.
-//
-#include "state.h"
-
-void setupWifi();
-void waitForWifi();
-void announceWifi();
-void request(State *state);
